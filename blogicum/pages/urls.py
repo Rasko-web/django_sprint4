efd38fea2ee46e9c6480e746f74a489f4b1eea16
Георгий Петухов <gosha_pet@mail.ru>
2023-07-20 10:45:56 +0300

    Verison 1.1

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -7,8 +7,7 @@ urlpatterns = [
     path(
         'about/',
         TemplateView.as_view(template_name='pages/about.html'),
-        name='about'
-        ),
+        name='about'),
     path(
         'rules/',
         TemplateView.as_view(template_name='pages/rules.html'),
